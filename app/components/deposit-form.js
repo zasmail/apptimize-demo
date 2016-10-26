92228495221be0e5f4842f36bbb48c7d5ef64f71
dev <dev@Zohairs-MacBook-Pro.local>
2016-10-26 13:00:32 -0700

    Styled for apptimize

--- app/components/deposit-form.js
+++ app/components/deposit-form.js
@@ -12,15 +12,6 @@ export default Ember.Component.extend({
   company:  null,
   name: null,
 
-  nameObserver: Ember.on('didInsertElement', Ember.observer('name', function(){
-    if(this.get('name') != null){
-      this.set('firstName', this.get('name').split(' ')[0]);
-      if(this.get('name').split(' ').length > 1){
-        this.set('lastName', this.get('name').split(' ')[1]);
-      }
-    }
-  })),
-
   actions:{
     changedSelection: function(value){
       this.set('amount', value.amount);
